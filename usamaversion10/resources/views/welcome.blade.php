c4dab211f9023e28e30a1fb5263e71696e4ea3da
usamapbx <usamapbx1@gmail.com>
2024-06-12 11:27:13 +0500

    hgjhg

---
+++ usamaversion10/resources/views/welcome.blade.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .home{
+            font-family: sans-serif;
+            color: #505f98;
+            text-decoration: none;
+        }
+        .about{
+            font-family: sans-serif;
+            color: #505f98;
+            text-decoration: none;
+
+        }
+        .container{
+            margin-left: 200px;
+        }
+        .contact{
+            font-family: sans-serif;
+            color: #505f98;
+            text-decoration: none;
+
+        }
+        h1{
+            font-family: sans-serif;
+            margin-top: 150px;
+            margin-top: 150px;
+            margin-left: 140px;
+        }
+        .pathh{
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+        .groupp{
+            position: absolute;
+            top: 0;
+            right: 0;
+            margin-top: 200px
+        }
+        .para{
+            margin-top: 150px;
+            margin-left: 140px;
+            font-family: sans-serif;
+
+        }
+        .button{
+            background-color: #111b47;
+            color: azure;
+            border-radius: 2px;
+            text-align: center;
+            position: absolute;
+            top: 0;
+            height: 26px;
+            width: 160px;
+            right: 0;
+            margin-top: 20px;
+            margin-right: 90px;
+        }
+        .pic2{
+           position: absolute;
+           right: 0;
+           top: 0;
+           margin-top: 1200px;
+           margin-right: 80px;
+        }
+        .ndheading{
+            font-family: sans-serif; 
+            margin-top: 800px;
+
+        }
+        .p2{
+            font-family: sans-serif;
+            margin-left: 140px;
+        }
+        .logoimages{
+            margin-left: 140px;
+            margin-top: 100px;
+        }
+        .logoh1{
+            font-family: sans-serif;
+            margin-left: 140px;
+            margin top: 20px;
+        }
+        .logoh2{
+            font-family: sans-serif;
+            margin-left: 430px;
+            margin top: 20px;
+        }
+        .p3{
+            font-family: sans-serif;
+            margin-left: 140px;
+            margin-right: 1050px;
+        }
+        .p4{
+            font-family: sans-serif;
+            margin-left: 280px;
+            margin-right: 500px;
+        }
+        .rdheading{
+            font-family: sans-serif; 
+            margin-left: 900px;
+            margin-top: -250px;
+        }
+        .thheading{
+            font-family: sans-serif; 
+            margin-left: 900px;
+            margin-top: -250px;
+        }
+        .pic4{
+            margin-top: 330px;
+        }
+        .para3{
+            font-family: sans-serif; 
+            margin-left: 900px;
+            margin-top: 30px;
+            padding-right: 30px;
+        }
+        .para4{
+            font-family: sans-serif; 
+            margin-left: 900px;
+            margin-top: 30px;
+            padding-right: 30px;
+        }
+        .purchasbutton{
+            background-color: #111b47;
+            color: azure;
+            border-radius: 2px;
+            text-align: center;
+            height: 32px;
+            width: 150px;
+            position: absolute;
+            right: 0;
+            margin-right: 350px;
+        }
+        .footer{
+            margin-top: -200px;
+        }
+    </style>
+</head>
+<body>
+    <div class="parent">
+        <img class="pathh" src="images/path.svg" alt="ishaq">
+        <img class="groupp"src="images/group.svg" alt="ishaq">
+    </div>
+    <div class="container">
+        <a class="home" href="/">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a class="about" href="/about">About</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a class="contact" href="/contact">Contact</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </div>
+   <input class="button" type="text" value="Buy Now" >
+
+   
+    <h1 class=>Introduce Your Product <br>Quickly & Effectively</h1>
+    <p class="para">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br>Lorem Ipsum has been the industry's standard dummy text <br>eve but also the leap into electronic typesetting, remaining essentially <br> unchanged. It was the industry's standard dummy text eve but also the leap. </p>
+
+    <img class="pic2" src="images/pic 2.svg" alt="">
+    <h1 class="ndheading">Light,Fast & Powerful</h1>
+    <p class="p2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br>Lorem Ipsum has been the industry's standard dummy text <br><br>eve but also the leap into electronic typesetting, remaining essentially <br> unchanged. It was the industry's standard dummy text eve but also the leap. </p>
+    
+    <div class="logoimages">
+        <img class="imagel" src="images/left.svg" alt="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <img class="imageR" src="images/right.svg" alt="">
+    </div>
+    
+        <h3 class="logoh1">Title Goes Here  </h3>
+        <h3 class="logoh2">Title Goes Here  </h3>
+<br><br>
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br>
+<br>
+
+
+ <img src="images/3rd pic.svg" alt="">
+<h1 class="rdheading">Light,Fast & Powerful</h1>
+<p class="para3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br>Lorem Ipsum has been the industry's standard dummy text <br>eve but also the leap into electronic typesetting, remaining essentially <br> unchanged. It was the industry's standard dummy text eve but also the leap. </p>
+
+<img class="pic4" src="images/pic4.svg" alt="">
+<h1 class="thheading">Light,Fast & Powerful</h1>
+<p class="para4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br>Lorem Ipsum has been the industry's standard dummy text <br>eve but also the leap into electronic typesetting, remaining essentially <br> unchanged. It was the industry's standard dummy text eve but also the leap. </p>
+<input class="purchasbutton" type="text" value="Purchase Now "><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<img src="images/pricing.svg" alt="">
+<img class="footer" src="images/footer.svg" alt="dfghjk">
+</body>
+
+    
+
+
+</html>
